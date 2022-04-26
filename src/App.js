2d79cd2712f389458b19841e21715e39b32f7c1d
coolpandadev <coolpanda1219@gmail.com>
2022-04-26 10:37:26 -0500

    Display raw airlines data in html table

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import data from './data';
+
 const App = () => (
   <div className="app">
   <header className="header">
     <h1 className="title">Airline Routes</h1>
   </header>
   <section>
-    <p>
-      Welcome to the app!
-    </p>
+    <table className="routes-table">
+      <thead>
+        <tr>
+          <th>Airline</th>
+          <th>Source Airport</th>
+          <th>Destination Airport</th>
+        </tr>
+      </thead>
+      {data.routes.map(route => {
+        const id = `${route.airline}-${route.src}-${route.dest}` // makeshift id
+        return (
+          <tr key={id}>
+            <td>{route.airline}</td>
+            <td>{route.src}</td>
+            <td>{route.dest}</td>
+          </tr>
+        );
+      })}
+    </table>
   </section>
 </div>
-)
+);
 
 export default App;
